965c5d922b018fc1f3d8b1b97e62aa9ae198649e
Jorrell Smith <sabatiel180@gmail.com>
2025-11-21 14:22:25 -0500

    third-attempt

--- install-deps.sh
+++ install-deps.sh
@@ -5,7 +5,7 @@ if ! [yum list installed "$DOCKER_INSTALL" &>/dev/null]; then
     sudo yum install -y yum-utils device-mapper-persistent-data lvm2
     sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
     sudo yum install docker-ce docker-ce-cli containerd.io
-    sudo systemctl start docker
-    sudo systemctl enable docker
+    sudo systemctl start docker-ce
+    sudo systemctl enable docker-ce
     sudo docker run hello-world
 fi
