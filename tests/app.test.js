51288b9656c5d23e297c07fd17257ba16a70bea6
Jorrell Smith <sabatiel180@gmail.com>
2025-11-25 09:54:41 -0600

    ok

--- tests/app.test.js
+++ tests/app.test.js
@@ -4,8 +4,17 @@ const app = require('../app');
 
 describe('Event API', () => {
     it('should return an error on the event trigger', async () => {
-        const res = await request(app).get('/api/start');
+        const event = { event: 'test', ticker: null };
+
+        const res = await request(app)
+        .post('/api/start')
+        .send(event)
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(201);
+        
         expect(res.statusCode).toEqual(201);
         expect(res.body).toBeInstanceOf(Array);
+        expect(res.body.message).toEqual("Could not process the event")
     });
 });
