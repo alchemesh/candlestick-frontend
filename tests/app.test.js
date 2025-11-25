500f85fef2d41568592ac78954bf5b738cda7056
Jorrell Smith <sabatiel180@gmail.com>
2025-11-25 10:01:37 -0600

    ok

--- tests/app.test.js
+++ tests/app.test.js
@@ -2,7 +2,7 @@
 const request = require('supertest');
 const app = require('../app');
 
-describe('Event API', () => {
+describe('Event API', async () => {
     it('should return an error on the event trigger', async () => {
         const event = { event: 'test', ticker: null };
 
