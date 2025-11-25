b05c6ec7f1329e2203cd7c1b70e3578521c42b01
Jorrell Smith <sabatiel180@gmail.com>
2025-11-25 10:28:52 -0600

    using describe

--- tests/app.test.js
+++ tests/app.test.js
@@ -2,19 +2,19 @@
 const request = require('supertest');
 const app = require('../app');
 
-test('Event API', async () => {
-    //it('should return an error on the event trigger', async () => {
-    const event = { event: 'test', ticker: null };
+describe ('Event API', () => {
+    it('should return an error on the event trigger', async () => {
+        const event = { event: 'test', ticker: null };
 
-    const res = await request(app)
-    .post('/api/start')
-    .send(event)
-    .set('Accept', 'application/json')
-    .expect('Content-Type', /json/)
-    .expect(201);
-    
-    expect(res.statusCode).toEqual(201);
-    expect(res.body).toBeInstanceOf(Object);
-    expect(res.body.message).toEqual("Could not process the event")
-    //});
+        const res = await request(app)
+        .post('/api/start')
+        .send(event)
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(201);
+        
+        expect(res.statusCode).toEqual(201);
+        expect(res.body).toBeInstanceOf(Object);
+        expect(res.body.message).toEqual("Could not process the event")
+    });
 });
